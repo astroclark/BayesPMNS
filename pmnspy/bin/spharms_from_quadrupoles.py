ed7236a3ac035115d966fd5ba55cd33b2d24d0f3
James Clark <james.clark@ligo.org>
2015-02-18 14:30:47 -0500

    fixing distance; should double check this one day..

--- pmnspy/bin/spharms_from_quadrupoles.py
+++ pmnspy/bin/spharms_from_quadrupoles.py
@@ -123,13 +123,14 @@ times -= times[0]
 #                         #
 ###########################
 if opts.ninja_units:
+
     # Just rescale to 1 Mpc
-    Ixx *= extract_dist
-    Ixy *= extract_dist
-    Ixz *= extract_dist
-    Iyy *= extract_dist
-    Iyz *= extract_dist
-    Izz *= extract_dist
+    Ixx *= 1.0#extract_dist
+    Ixy *= 1.0#extract_dist
+    Ixz *= 1.0#extract_dist
+    Iyy *= 1.0#extract_dist
+    Iyz *= 1.0#extract_dist
+    Izz *= 1.0#extract_dist
 
 else:
     # Convert to NINJA units
