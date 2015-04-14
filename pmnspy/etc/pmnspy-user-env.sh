b66d9d55594a8d6fea6ad99d8015434e25c1f3aa
James Clark <james.clark@ligo.org>
2015-04-14 11:36:46 -0400

    removed config file

--- pmnspy/etc/pmnspy-user-env.sh
+++
@@ -1,7 +0,0 @@
-# add script directory to path
-export PATH=/home/jclark/Projects/BayesPMNS/pmnspy/bin:$PATH
-export PYTHONPATH=/home/jclark/Projects/BayesPMNS/pmnspy/bin:/home/jclark/Projects/BayesPMNS/pmnspy/pmns_utils:${PYTHONPATH}
-# define variable for location of ninja ascii files
-export NINJA_ASCII=/home/jclark/Projects/BayesPMNS/pmnspy/../waveform_data/ninja_ascii
-export SECDERIVS=/home/jclark/Projects/BayesPMNS/pmnspy/../waveform_data/secderivqpoles
-export PMNSPY_PREFIX=/home/jclark/Projects/BayesPMNS/pmnspy
