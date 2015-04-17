68da72c222bd2550e559da4e6f7b67acbbc9722f
James Clark <james.clark@ligo.org>
2015-04-17 18:14:18 -0400

    all the plots

--- pmnspy/pca_demos/pmns_plotpcs.py
+++ pmnspy/pca_demos/pmns_plotpcs.py
@@ -35,21 +35,6 @@ from sklearn.decomposition import PCA
 n_row, n_col = 2, 3
 n_components = n_row*n_col
 
-def plot_tfpcs(title, images, n_col=n_col, n_row=n_row):
-    pl.figure(figsize=(4. * n_col, 4.52 * n_row))
-    pl.suptitle(title, size=16)
-    for i, comp in enumerate(images):
-        pl.subplot(n_row, n_col, i + 1)
-
-        vmin, vmax = -comp.max(), comp.max()
-        collevs=np.linspace(vmin, vmax, 100)
-        pl.contourf(times-0.5*max(times), frequencies,
-                comp.reshape(image_shape), cmap=pl.cm.gnuplot2_r, levels=collevs)
-        pl.xlim(-0.005,0.025)
-        pl.ylim(min(frequencies),
-                2*frequencies[abs(scales-0.5*max(scales)).argmin()])
-
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # THINGS TO PLOT IN THIS SCRIPT
 #
@@ -124,9 +109,12 @@ def plot_freqpc(ax,x,y,xlims=(999,4096),ylims=(0, 0.035)):
 
     return yticks
 
-def plot_timefreqmap(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(999,4096)):
+def plot_timefreqmap(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(999,4096), clims=None):
 
-    vmin, vmax = 0, z.max()
+    if clims is None:
+        vmin, vmax = 0, z.max()
+    else:
+        vmin, vmax = clims
     collevs=np.linspace(vmin, vmax, 100)
     cont = ax.contourf(x-0.5*x.max(), y, z, cmap=pl.cm.gnuplot2_r, levels=collevs)
 
@@ -148,11 +136,16 @@ def plot_timefreqmap(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(999,4096)):
 
     return yticks, cont
 
-def plot_timefreqpc(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(128,300)):
+def plot_timefreqpc(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(128,300),
+        clims=None):
 
-    vmin, vmax = -z.max(), z.max()
+    if clims is None:
+        vmin, vmax = -z.max(), z.max()
+    else:
+        vmin, vmax = clims
+
     collevs=np.linspace(vmin, vmax, 100)
-    ax.contourf(x-0.5*x.max(), y, z, cmap=pl.cm.gnuplot2_r, levels=collevs)
+    tfmap = ax.contourf(x-0.5*x.max(), y, z, cmap=pl.cm.gnuplot2_r, levels=collevs)
 
     ax.set_xlim(xlims)
     ax.set_ylim(ylims)
@@ -167,7 +160,7 @@ def plot_timefreqpc(ax, x, y, z, xlims=(-0.0025,0.025), ylims=(128,300)):
     ax.set_xticks(xticks)
     ax.grid()
 
-    return 0
+    return tfmap
 
 #   pl.xlim(-0.005,0.025)
 #   pl.ylim(min(frequencies),
@@ -205,12 +198,15 @@ pmpca = ppca.pmnsPCA(waveform_names, low_frequency_cutoff=1000, fcenter=2710,
 nexamples=3
 fcat, axcat = pl.subplots(ncols=nexamples, nrows=3, figsize=(10,8))
 for w in xrange(3):
+
     tyticks  = plot_timeseries(axcat[0][w], pmpca.sample_times, pmpca.cat_timedomain[w,:])
     fyticks  = plot_freqseries(axcat[1][w], pmpca.sample_frequencies, abs(pmpca.cat_orig[w,:]))
-    tfyticks, tfmap = plot_timefreqmap(axcat[2][w], pmpca.map_times, pmpca.map_frequencies, pmpca.original_image_cat[w])
+    tfyticks, tfmap = plot_timefreqmap(axcat[2][w], pmpca.map_times,
+            pmpca.map_frequencies, pmpca.original_image_cat[w])
 
     c = pl.colorbar(tfmap, orientation='horizontal', ax=axcat[2][w],
             shrink=0.9, pad=.25)
+    c.set_ticks(np.arange(0,1,0.1))
 
 
 
@@ -225,11 +221,10 @@ axcat[2][0].set_ylabel('Frequency [Hz]')
 axcat[2][0].set_yticklabels(tfyticks)
 
 
+fcat.suptitle('Example Waveforms')
 fcat.tight_layout()
 fcat.subplots_adjust(wspace=0.0)
 
-pl.show()
-sys.exit()
 
 #
 # Plot Explained Variance
@@ -272,8 +267,13 @@ times = pmpca.map_times
 freqs = pmpca.map_frequencies
 image_shape = pmpca.original_image_cat[0].shape
 meanmap = pmpca.pca['timefreq_mean'].reshape(image_shape)
-ax[1].contourf(times-0.5*times.max(), freqs, meanmap, cmap=pl.cm.gnuplot2_r,
+
+cont = ax[1].contourf(times-0.5*times.max(), freqs, meanmap, cmap=pl.cm.gnuplot2_r,
         levels=collevs)
+
+c = pl.colorbar(cont, orientation='horizontal', ax=ax[1],
+        shrink=0.9, pad=.25)
+c.set_ticks(np.arange(0,1,0.1))
 
 xlims=(-0.0025,0.025)
 ylims=(128,300)
@@ -286,6 +286,7 @@ ax[1].grid()
 ax[1].set_xlabel('Time [s]')
 ax[1].set_ylabel('Frequency [arb. units]')
 
+f.suptitle('Global Averages')
 f.tight_layout()
 
 #
@@ -295,15 +296,23 @@ f, ax = pl.subplots(nrows=2, ncols=3, figsize=(10,5))
 for w in xrange(3):
     fyticks = plot_freqpc(ax[0][w], pmpca.sample_frequencies,
             pmpca.magnitude_align[w,:] - pmpca.pca['magnitude_mean'])
-    ax[0][w].set_ylim(-0.01,0.01)
 
-    plot_timefreqpc(ax[1][w], pmpca.map_times, pmpca.map_frequencies,
-            pmpca.align_image_cat[w])
+    #ax[0][w].set_ylim(-0.01,0.01)
+
+    clims = (-0.4,0.4)
+    tfmap = plot_timefreqpc(ax[1][w], pmpca.map_times, pmpca.map_frequencies,
+            pmpca.align_image_cat[w]-pmpca.pca['timefreq_mean'].reshape(image_shape),
+            clims=clims)
+
+    c = pl.colorbar(tfmap, orientation='horizontal', ax=ax[1][w],
+            shrink=0.9, pad=.25)
+    c.set_ticks(np.arange(clims[0],clims[1]+0.2,0.2))
 
 ax[0][0].set_yticklabels(fyticks)
 
 f.tight_layout()
 f.subplots_adjust(wspace=0.0)
+f.suptitle('Centered Data')
 
 pl.show()
 
@@ -313,33 +322,23 @@ pl.show()
 f, ax = pl.subplots(nrows=2, ncols=3, figsize=(10,5))
 for w in xrange(3):
 
-    plot_freqpc(ax[0][w], pmpca.sample_frequencies,
+    fyticks = plot_freqpc(ax[0][w], pmpca.sample_frequencies,
             pmpca.pca['magnitude_pca'].components_[w,:])
 
+    clims = (-0.025,0.025)
+    tfmap = plot_timefreqpc(ax[1][w], pmpca.map_times, pmpca.map_frequencies,
+            pmpca.pca['timefreq_pca'].components_[w,:].reshape(image_shape),clims=clims)
 
-    plot_timefreqpc(ax[1][w], pmpca.map_times, pmpca.map_frequencies,
-            pmpca.pca['timefreq_pca'].components_[w,:].reshape(image_shape))
+    c = pl.colorbar(tfmap, orientation='horizontal', ax=ax[1][w],
+            shrink=0.9, pad=.25)
+
+    c.set_ticks(np.arange(-1,1,0.01))
 
 ax[0][0].set_ylabel('PC amplitude')
-ax[1][0].set_ylabel('Frequency [arb. units]')
+ax[0][0].set_yticklabels(fyticks)
+ax[1][0].set_ylabel('Frequency \n [arb. units]')
 
+f.suptitle('Principal Components (1-3)')
 f.tight_layout()
 f.subplots_adjust(wspace=0.0)
-
-pl.show()
-sys.exit()
-
-#
-# Plot TF PCs
-#
-
-times = pmpca.map_times
-frequencies = pmpca.map_frequencies
-scales = pmpca.map_scales
-
-plot_tfpcs(pmpca.pca['timefreq_pca'].components_[:n_components])
-
-
-pl.show()
-
 
