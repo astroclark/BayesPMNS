29712f7f59a6163051f2589a01127f4909afce5e
James Clark <james.clark@ligo.org>
2015-06-16 10:12:18 -0400

    changing location of waveform data

--- pmnspy/setup.sh
+++ pmnspy/setup.sh
@@ -10,14 +10,12 @@ PMNSPY_PREFIX=`python -c "import os, sys; print os.path.realpath('${0}')" | sed 
 test -d "${PMNSPY_PREFIX}/etc" || mkdir "${PMNSPY_PREFIX}/etc"
 
 # define a variable to point to the NINJA ascii data.  This is useful for waveform generation scripts.
-NINJA_ASCII="${PMNSPY_PREFIX}/../waveform_data/ninja_ascii"
-SECDERIVS="${PMNSPY_PREFIX}/../waveform_data/secderivqpoles"
+PMNSPATH="${PMNSPY_PREFIX}/../waveform_data"
 
 echo "# add script directory to path" > "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
 echo "export PATH=$PMNSPY_PREFIX/bin:\$PATH" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
 echo "export PYTHONPATH=$PMNSPY_PREFIX/bin:$PMNSPY_PREFIX/pmns_utils:\${PYTHONPATH}" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
-echo "# define variable for location of ninja ascii files" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
-echo "export NINJA_ASCII=${NINJA_ASCII}" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
-echo "export SECDERIVS=${SECDERIVS}" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
+echo "# define variable for location of waveform data" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
+echo "export PMNSPATH=${PMNSPATH}" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
 echo "export PMNSPY_PREFIX=${PMNSPY_PREFIX}" >> "${PMNSPY_PREFIX}/etc/pmnspy-user-env.sh"
 
