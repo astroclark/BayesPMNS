7392e5726ce1194f115f1e876c7c5d826dad4f48
James Clark <james.clarrk@ligo.org>
2016-08-09 15:45:42 -0400

    getting closer

--- pmnspy/nrhdf5/pmns_modes.py
+++ pmnspy/nrhdf5/pmns_modes.py
@@ -28,11 +28,13 @@ from matplotlib import pyplot as pl
 
 import pycbc.types
 from pycbc.waveform import utils as wfutils
+import lal
 
 from pmns_utils import pmns_waveform as pwave
 from pmns_utils import pmns_waveform_data as pdata
 
-import romspline
+import h5py
+import romspline as romSpline
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -41,6 +43,7 @@ import romspline
 
 eos="tm1"
 mass="135135"
+total_mass=2*1.35
 
 # Hardcoded, fixed delta_t is fine for Bauswein et al:
 delta_t = 1./16384
@@ -48,7 +51,6 @@ delta_t = 1./16384
 #
 # Create waveform catalog (EOS, mass & file path)
 #
-
 waveform_data = pdata.WaveData(eos=eos, mass=mass, viscosity='lessvisc')
 
 #
@@ -60,19 +62,7 @@ quadrupole_data = pwave.get_quadrupole_data(waveform_data.waves[0]['data'])
 # Generate Hlm's
 #
 
-# Hlm is a dictionary:
-#In [8]: Hlm.keys()
-#Out[8]: ['l=2, m=2', 'l=2, m=-2']
 Hlm = pwave.construct_Hlm(*quadrupole_data[1:])
-
-# Turn this into amp/phase 
-HlmAmp   = dict()
-HlmPhase = dict()
-for key in Hlm.keys():
-    hplus  = pycbc.types.TimeSeries(np.real(Hlm[key]), delta_t=delta_t)
-    hcross = pycbc.types.TimeSeries(-1*np.imag(Hlm[key]), delta_t=delta_t)
-    HlmAmp[key] = wfutils.amplitude_from_polarizations(hplus, hcross)
-    HlmPhase[key] = wfutils.phase_from_polarizations(hplus, hcross)
 
 # ROMspline usage:
 #    spline = romSpline.ReducedOrderSpline(time_data, amplitude/phase_data,
@@ -86,6 +76,41 @@ for key in Hlm.keys():
 
 # 3) use examples in lalapps_nest2pos to make hdf5 groups for output files:
 # https://github.com/johnveitch/lalsuite/blob/h5utils/lalapps/src/inspiral/posterior/lalapps_nest2pos.py#L155
+
+wavelen=len(Hlm['l=2, m=2'])
+times=np.arange(0, wavelen*delta_t, delta_t)
+times_M = times / (lal.MTSUN_SI * total_mass)
+
+
+# GEOMETERIZE TIMES AND HLM
+
+l=2
+with h5py.File(eos+'_'+mass+'.h5','w') as fd:
+
+    for m in range(-l,l+1):
+
+        if abs(m)!=2:
+            HlmAmp=np.zeros(wavelen)
+            HlmPhase=np.zeros(wavelen)
+        else:
+            key="l=%d, m=%d"%(l,m)
+
+            hplus  = pycbc.types.TimeSeries(np.real(Hlm[key]), delta_t=delta_t)
+            hcross = pycbc.types.TimeSeries(-1*np.imag(Hlm[key]), delta_t=delta_t)
+
+            HlmAmp   = wfutils.amplitude_from_polarizations(hplus, hcross).data
+            HlmPhase = wfutils.phase_from_polarizations(hplus, hcross).data
+
+        sAmph = romSpline.ReducedOrderSpline(times, HlmAmp, verbose=False)
+        sPhaseh = romSpline.ReducedOrderSpline(times, HlmPhase, verbose=False)
+      
+        gramp = fd.create_group('amp_l%d_m%d' %(l,m))
+        sAmph.write(gramp)
+        
+        grphase = fd.create_group('phase_l%d_m%d' %(l,m))
+        sPhaseh.write(grphase)
+
+#    <some syntax for closing / writing the hdf5 file with fd>
 
 
 
