fa711e547cba38746ee4d32fec1ec39edad5c07e
James Clark <james.clark@ligo.org>
2016-11-09 20:17:42 -0500

    CAN NOW DO ALL WAVEFORMS

--- pmnspy/nrhdf5/pmns_trento_modes.py
+++ pmnspy/nrhdf5/pmns_trento_modes.py
@@ -24,16 +24,17 @@ from __future__ import division
 import os,sys
 import hashlib
 import numpy as np
+import glob
 
 from matplotlib import pyplot as pl
 
 import pycbc.types
 from pycbc.waveform import utils as wfutils
 from pycbc import pnutils
+#import pycbc.filter
+import scipy.signal
 import lal
 
-from pmns_utils import pmns_waveform as pwave
-from pmns_utils import pmns_waveform_data as pdata
 
 import h5py
 #import romspline as romSpline
@@ -44,53 +45,27 @@ import romSpline
 # Waveform Generation
 #
 
-eos="apr"
-mass="135135"
-total_mass=2*1.35
-mass1=1.35
-mass2=1.35
+eos="apr4"
+mass="142159"
+total_mass=1.42+1.59
+mass1=1.42
+mass2=1.59
+
+datadir='/home/jclark/Projects/gwaveforms/BNS/APR4/mb_1.42_1.59_d45/bns_apr4_4p_mb_1.42_1.59_irrot.ninja'
+
+globpattern='bns_apr4_4p_mb_1.42_1.59_irrot*asc'
+ninjafiles=glob.glob(os.path.join(datadir,globpattern))
 
 # Hardcoded, fixed delta_t is fine for Bauswein et al:
 delta_t = 1./16384
 f_lower_hz = 1000.0 # waveform not valid below here really; could put this on the
-                 # pipeline to handle...
+                    # pipeline to handle...
+
+
+
 #startFreqHz = startFreq / (lal.TWOPI * massTotal * lal.MTSUN_SI)
 f_lower = f_lower_hz * (lal.TWOPI * total_mass * lal.MTSUN_SI)
 
-#
-# Create waveform catalog (EOS, mass & file path)
-#
-waveform_data = pdata.WaveData(eos=eos, mass=mass, viscosity='lessvisc')
-
-#
-# Example waveform
-#
-waveform = pwave.Waveform(eos=eos, mass=mass,
-        viscosity='lessvisc', distance=100)
-waveform.reproject_waveform()
-
-
-
-#
-# Pull out quadrupole data
-#
-quadrupole_data = pwave.get_quadrupole_data(waveform_data.waves[0]['data'])
-
-#
-# Generate Hlm's
-#
-
-Hlm = pwave.construct_Hlm(*quadrupole_data[1:])
-
-
-
-wavelen=len(Hlm['l=2, m=2'])
-times=np.arange(0, wavelen*delta_t, delta_t)
-times_M = times / (lal.MTSUN_SI * total_mass)
-# GEOMETERIZE  HLM (might be already!) see line 241+ of pmns_waveform.py
-
-
-l=2
 with h5py.File(eos+'_'+mass+'.h5','w') as fd:
 
     #
@@ -98,9 +73,9 @@ with h5py.File(eos+'_'+mass+'.h5','w') as fd:
     #
 
     mchirp, eta = pnutils.mass1_mass2_to_mchirp_eta(mass1, mass2)
-    fd.attrs.create('NR_group', 'Bauswein')
-    fd.attrs.create('name', 'Bauswein:BNS:%s' %
-            (waveform_data.eos+'_'+waveform_data.mass+'_'+waveform_data.viscosity))
+    fd.attrs.create('NR_group', 'Trento')
+    fd.attrs.create('name', 'Trento:BNS:%s' %
+            (eos+'_'+mass))
     hashtag = hashlib.md5()
     hashtag.update(fd.attrs['name'])
     fd.attrs.create('hashtag', hashtag.digest())
@@ -117,21 +92,47 @@ with h5py.File(eos+'_'+mass+'.h5','w') as fd:
     fd.attrs.create('mass2', mass2/total_mass)
 
 
-    for m in range(-l,l+1):
+    for ninjafile in ninjafiles:
+        print ninjafile
 
-        if abs(m)!=2:
-            HlmAmp=np.zeros(wavelen)
-            HlmPhase=np.zeros(wavelen)
-        else:
-            key="l=%d, m=%d"%(l,m)
+        lm = ninjafile.split('_')[-1].replace('.asc','')
+        l=float(lm.split('m')[0].replace('l',''))
+        m=float(lm.split('m')[-1])
 
-            hplus  = pycbc.types.TimeSeries(np.real(Hlm[key]), delta_t=delta_t)
-            hcross = pycbc.types.TimeSeries(-1*np.imag(Hlm[key]), delta_t=delta_t)
+        timesGeom, hplusGeom, hcrossGeom = np.loadtxt(ninjafile, unpack=True)
 
-            massMpc =  total_mass * lal.MRSUN_SI / (  20.0e6)
-            HlmAmp   = massMpc*wfutils.amplitude_from_polarizations(hplus, hcross).data
-            HlmPhase = wfutils.phase_from_polarizations(hplus, hcross).data 
+        #
+        # Reverse engineer for sanity check and resampling
+        #
+        scalefac = lal.MRSUN_SI / (100e6*lal.PC_SI)
+        times = timesGeom*lal.MTSUN_SI
 
+        native_delta_t = np.diff(times)[0]
+        hplus = hplusGeom*scalefac
+        hcross = hcrossGeom*scalefac
+
+        target_nsamp = times[-1]/delta_t
+        hplus_16384, times_16384 = scipy.signal.resample(hplus, num=target_nsamp,
+                t=times)
+        hcross_16384, _ = scipy.signal.resample(hcross, num=target_nsamp,
+                t=times)
+        times_16384 = times_16384[:len(hplus_16384)]
+
+        massMpc = total_mass*lal.MRSUN_SI / (lal.PC_SI*100.0e6)
+
+        hplusMpc  = pycbc.types.TimeSeries(hplus/massMpc, delta_t=delta_t)
+        hcrossMpc = pycbc.types.TimeSeries(hcross/massMpc, delta_t=delta_t)
+        times_M = times / (lal.MTSUN_SI * total_mass)
+ 
+        HlmAmp   = wfutils.amplitude_from_polarizations(hplusMpc,
+                hcrossMpc).data
+        HlmPhase = wfutils.phase_from_polarizations(hplusMpc, hcrossMpc).data 
+
+        if l!=2 or m!=2:
+            HlmAmp = np.zeros(len(HlmAmp))
+            HlmPhase = np.zeros(len(HlmPhase))
+ 
+        print 'fitting spline'
         sAmph = romSpline.ReducedOrderSpline(times_M, HlmAmp, verbose=False)
         sPhaseh = romSpline.ReducedOrderSpline(times_M, HlmPhase, verbose=False)
       
@@ -141,6 +142,7 @@ with h5py.File(eos+'_'+mass+'.h5','w') as fd:
         grphase = fd.create_group('phase_l%d_m%d' %(l,m))
         sPhaseh.write(grphase)
 
+ 
 
 
 
